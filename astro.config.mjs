5c914cbd05ae60330e35352c56b4c3f5f1078d6f
Pawel Sobaniec <infipablosoban@gmail.com>
2023-01-15 15:46:18 +0100

    Work on spotify stats

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,16 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), image()]
+  integrations: [tailwind(), mdx(), image(), react()],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  })
 });
