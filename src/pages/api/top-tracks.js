d1899c048c1ee38fc1e4cec9615f2d664aaf7cb3
Pawel Sobaniec <infipablosoban@gmail.com>
2023-01-15 16:17:23 +0100

    Undo change js to ts

--- src/pages/api/top-tracks.js
+++ src/pages/api/top-tracks.js
@@ -1,12 +1,11 @@
 import { getTopTracks } from "../../lib/spotify";
-import type { APIRoute } from 'astro';
 
 const defaultHeader = {
   'Content-Type': 'application/json',
   'Cache-Control': 's-maxage=1, stale-while-revalidate=59',
 };
 
-export const get:APIRoute = async ({ request }) => {
+export const get = async ({ _, request }) => {
     if (request.method !== 'GET') {
       return new Response(undefined, { status: 400 });
     }
@@ -14,8 +13,8 @@ export const get:APIRoute = async ({ request }) => {
     const response = await getTopTracks();
     const { items } = await response.json();
   
-    const tracks = items.slice(0, 10).map((track:any) => ({
-      artist: track.artists.map((_artist:any) => _artist.name).join(', '),
+    const tracks = items.slice(0, 10).map((track) => ({
+      artist: track.artists.map((_artist) => _artist.name).join(', '),
       songUrl: track.external_urls.spotify,
       title: track.name
     }));
